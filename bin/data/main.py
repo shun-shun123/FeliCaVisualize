8d3394b2a8de42c3af0b2dfb33c1c122c3e047d1
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-08-29 23:52:45 +0900

    GETまで完了

--- bin/data/main.py
+++ bin/data/main.py
@@ -1,20 +1,6 @@
 # coding: utf-8
-import urllib.request
-import json
-import codecs
-import scrape
+from httpClient import HttpClient
 
-def httpGet():
-    url = 'https://script.google.com/macros/s/AKfycbxWAZpk0m3Xe3CWt80IZwncvgjowlT4FTdannJlaHofOFUGcKcC/exec'
-    request = urllib.request.Request(url)
-    response = urllib.request.urlopen(request)
-    content = json.loads(response.read().decode('utf-8'))
-    return content
+httpClient = HttpClient()
 
-def loadData():
-    data = httpGet()
-    with codecs.open("data.json", "w", "utf-8") as target:
-        json.dump(data, target, ensure_ascii=False, indent=4)
-
-
-loadData()
+httpClient.GET()
