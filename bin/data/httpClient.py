91dd30dec8a2d66fc9b1ca58a8d50524d0ed7362
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-08-29 23:52:30 +0900

    GETでデータを保存まで完了

--- bin/data/httpClient.py
+++ bin/data/httpClient.py
@@ -4,16 +4,18 @@ import json
 import codecs
 
 
-class HttpClient():
-    def httpGet():
+class HttpClient:
+    def httpGet(self):
         url = 'https://script.google.com/macros/s/AKfycbxWAZpk0m3Xe3CWt80IZwncvgjowlT4FTdannJlaHofOFUGcKcC/exec'
         request = urllib.request.Request(url)
         response = urllib.request.urlopen(request)
         content = json.loads(response.read().decode('utf-8'))
         return content
 
-    def loadData():
-        data = httpGet()
+    def loadData(self):
+        data = self.httpGet()
         with codecs.open("data.json", "w", "utf-8") as target:
             json.dump(data, target, ensure_ascii=False, indent=4)
 
+    def GET(self):
+        self.loadData()
