ec665243aa8608ca483d3516b1b0d2fbe0209021
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-08-30 00:15:24 +0900

    yahoo画像検索から画像取得に成功

--- bin/data/scrape.py
+++ bin/data/scrape.py
@@ -1,43 +1,49 @@
 # coding: UTF-8
 import urllib.request
 import bs4
+import os
 
-station_name = "向日町駅"
-url = "https://search.yahoo.co.jp/image/search?ei=UTF-8&fr=sfp_as&aq=-1&oq=&ts=8&p=" + urllib.parse.quote_plus(station_name, encoding="utf-8") + "&meta=vc%3D"
+class Pictures:
+    url = ""
+    station_name = ""
+    encoding_list = ["utf-8", "utf_8", "euc_jp","euc_jis_2004", "euc_jisx0213", "shift_jis",
+                    "shift_jis_2004","shift_jisx0213", "iso2022jp","iso2022_jp_1", "iso2022_jp_2",
+                    "iso2022_jp_3","iso2022_jp_ext","latin_1", "ascii"]
+    def __init__(self, station_name):
+        self.url = "https://search.yahoo.co.jp/image/search?ei=UTF-8&fr=sfp_as&aq=-1&oq=&ts=8&p=" + urllib.parse.quote_plus(station_name, encoding="utf-8") + "&meta=vc%3D"
+        self.station_name = station_name
+    
+    def __getHtml(self):
+        request = urllib.request.Request(self.url)
+        response = urllib.request.urlopen(request)
+        html = response.read()
+        html = self.__encodeHtml(html)
+        return html
+    
+    def __encodeHtml(self, html):
+        for enc in self.encoding_list:
+            try:
+                html.decode(enc)
+                break
+            except:
+                enc = None
+        if enc != None:
+            html = html.decode(enc)
+            return html
 
-request = urllib.request.Request(url)
-response = urllib.request.urlopen(request)
-html = response.read()
+    def scrapePictures(self):
+        resources = []
+        html = self.__getHtml()
+        soup = bs4.BeautifulSoup(html, "html.parser")
+        elements = soup.findAll('img')
+        self.__savePictures(elements)
 
-encoding_list = ["utf-8", "utf_8", "euc_jp", 
-                    "euc_jis_2004", "euc_jisx0213", "shift_jis",
-                    "shift_jis_2004","shift_jisx0213", "iso2022jp",
-                     "iso2022_jp_1", "iso2022_jp_2", "iso2022_jp_3",
-                    "iso2022_jp_ext","latin_1", "ascii"]
+    def __savePictures(self, elements):
+        num = 0
+        os.makedirs("./" + str(self.station_name), exist_ok=True)
+        for e in elements:
+            request = urllib.request.urlopen(e['src'])
+            with open("./" + str(self.station_name) + "/pic" + str(num) + ".png", "wb") as f:
+                f.write(request.read())
+            num += 1
 
-for enc in encoding_list:
-    try:
-        html.decode(enc)
-        break
-    except:
-        enc = None
-
-print(enc)
-if enc != None:
-    print(html.decode(enc))
-    html = html.decode(enc)
-
-resources = []
-
-# BeautifulSoupオブジェクト作成
-soup = bs4.BeautifulSoup(html)
-# htmlのすべてのaタグの中のhref属性の内容を取得
-elements = soup.findAll('img')
-print(type(elements))
-num = 0
-for e in elements:
-    request = urllib.request.urlopen(e['src'])
-    num += 1
-    f = open(str(num) + "pic.png", "wb")
-    f.write(request.read())
-    f.close()
