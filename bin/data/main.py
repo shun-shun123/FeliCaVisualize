e73e4453994cd26f32b35c6fb61b1e4d57c98590
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-08-30 00:15:39 +0900

    scrapePicturesを実行

--- bin/data/main.py
+++ bin/data/main.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 from httpClient import HttpClient
+from scrape import Pictures
 
 httpClient = HttpClient()
+scrapePictures = Pictures("長岡京駅")
 
 httpClient.GET()
+scrapePictures.scrapePictures()
