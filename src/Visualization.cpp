7917147739a22e750fc6a6de07d9f55b69b66ab5
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-08-30 22:20:50 +0900

    Visualizationのためのクラス作成

---
+++ src/Visualization.cpp
@@ -0,0 +1,8 @@
+//
+//  Visualization.cpp
+//  HackU2018
+//
+//  Created by 斉藤俊介 on 2018/08/30.
+//
+
+#include "Visualization.hpp"
---
+++ src/Visualization.hpp
@@ -0,0 +1,13 @@
+//
+//  Visualization.hpp
+//  HackU2018
+//
+//  Created by 斉藤俊介 on 2018/08/30.
+//
+
+#ifndef Visualization_hpp
+#define Visualization_hpp
+
+#include <stdio.h>
+
+#endif /* Visualization_hpp */
