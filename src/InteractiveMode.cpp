91f9deacbc5dcc4e5b0b37c667919b46918a0b48
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-08-30 18:19:44 +0900

    インタラクティブモードのクラス作成

---
+++ src/InteractiveMode.cpp
@@ -0,0 +1,8 @@
+//
+//  InteractiveMode.cpp
+//  HackU2018
+//
+//  Created by 斉藤俊介 on 2018/08/30.
+//
+
+#include "InteractiveMode.hpp"
---
+++ src/InteractiveMode.hpp
@@ -0,0 +1,13 @@
+//
+//  InteractiveMode.hpp
+//  HackU2018
+//
+//  Created by 斉藤俊介 on 2018/08/30.
+//
+
+#ifndef InteractiveMode_hpp
+#define InteractiveMode_hpp
+
+#include <stdio.h>
+
+#endif /* InteractiveMode_hpp */
