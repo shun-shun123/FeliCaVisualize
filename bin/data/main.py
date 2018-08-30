21c78bc486136b54e6522017f25bf9bbad96bf6e
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-08-30 18:17:45 +0900

    dataファイルの整理

--- bin/data/main.py
+++ bin/data/main.py
@@ -5,12 +5,12 @@ from scrape import Pictures
 httpClient = HttpClient()
 httpClient.GET()
 
-from jsonController import JsonController
-jsonController = JsonController()
-entranceStationNameList, exitStationNameList = jsonController.listOfStationName()
-for eachEntrance in entranceStationNameList:
-    scrapeEntrancePictures = Pictures(eachEntrance + "駅")
-    scrapeEntrancePictures.execute()
-for eachExit in exitStationNameList:
-    scrapeExitPictures = Pictures(eachExit + "駅")
-    scrapeExitPictures.execute()
+# from jsonController import JsonController
+# jsonController = JsonController()
+# entranceStationNameList, exitStationNameList = jsonController.listOfStationName()
+# for eachEntrance in entranceStationNameList:
+#     scrapeEntrancePictures = Pictures(eachEntrance + "駅")
+#     scrapeEntrancePictures.execute()
+# for eachExit in exitStationNameList:
+#     scrapeExitPictures = Pictures(eachExit + "駅")
+#     scrapeExitPictures.execute()
